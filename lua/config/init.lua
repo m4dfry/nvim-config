c8b498d1b7ff9b478ddef182eb19132822d6aa45
m4dfry <m4dfry@zoho.com>
2024-12-21 11:58:18 +0100

    Update plugins, re-enable ParenMatch

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,14 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -20,29 +20,29 @@ require("config.functions")
 require("config.diagnostic")
 
 local opts = {
-    defaults = {
-        lazy = true,
-    },
-    install = {
-        colorscheme = { "terafox" },
-    },
-    performance = {
-        rtp = {
-            disabled_plugins = {
-                "gzip",
-                "matchit",
-                "matchparen",
-                "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
-    change_detection = {
-        notify = true,
-    },
+	defaults = {
+		lazy = true,
+	},
+	install = {
+		colorscheme = { "terafox" },
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				-- "matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	change_detection = {
+		notify = true,
+	},
 }
 
 require("lazy").setup("plugins", opts)
