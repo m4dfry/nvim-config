49b98b1b8f0f9e9f0cf7b98f6cf63c559ba8cef5
m4dfry <m4dfry@zoho.com>
2025-06-06 12:38:59 +0200

    Fix LspConfig & Update?

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,52 +1,14 @@
 return {
-  "neovim/nvim-lspconfig",
-  lazy = false,
-  dependencies = {
-    "windwp/nvim-autopairs",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    dependencies = {
+        "windwp/nvim-autopairs",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
 
-  config = function()
-    require('java').setup()
-
-    local lspconfig = require("lspconfig")
-    local util = require("lspconfig/util")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-    })
-
-    lspconfig.gopls.setup({
-      cmd = { "gopls", "serve" },
-      filetypes = { "go", "gomod" },
-      root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-      settings = {
-        gopls = {
-          analyses = {
-            unusedparams = true,
-          },
-          staticcheck = true,
-        },
-      },
-    })
-
-    lspconfig.pyright.setup {
-      settings = {
-        pyright = {
-          autoImportCompletion = true,
-        },
-        python = {
-          analysis = {
-            autoSearchPaths = true,
-            diagnosticMode = 'openFilesOnly',
-            useLibraryCodeForTypes = true,
-            typeCheckingMode = 'off' }
-        }
-      }
-    }
-
-    lspconfig.jdtls.setup {}
-  end,
+    config = function()
+        vim.lsp.enable('pyright')
+        vim.lsp.enable('gopls')
+    end,
 }
